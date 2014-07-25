a750c809a855d0b71fab7ce6f124f9b1c9122049
sicongtang <bobby3239@gmail.com>
2014-07-25 16:30:15 +0800

    add node as REPL command

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -61,7 +61,8 @@
 ;; Run and interact with an inferior JS via js-comint.el
 ;; ---------------------------------------------------------------------------
 
-(setq inferior-js-program-command "js")
+;; Using node as a REPL tools
+(setq inferior-js-program-command "node --interactive")
 
 (defvar inferior-js-minor-mode-map (make-sparse-keymap))
 (define-key inferior-js-minor-mode-map "\C-x\C-e" 'js-send-last-sexp)
@@ -69,6 +70,23 @@
 (define-key inferior-js-minor-mode-map "\C-cb" 'js-send-buffer)
 (define-key inferior-js-minor-mode-map "\C-c\C-b" 'js-send-buffer-and-go)
 (define-key inferior-js-minor-mode-map "\C-cl" 'js-load-file-and-go)
+;; Send region to REPL tools
+(define-key inferior-js-minor-mode-map "\C-cr" 'js-send-region)
+(define-key inferior-js-minor-mode-map "\C-c\C-r" 'js-send-region-and-go)
+
+;; Using nodejs, we need to simply the output
+(setenv "NODE_NO_READLINE" "1")
+
+;; Replace nonsense message in REPL command prompt
+;;(setq inferior-js-mode-hook
+;;      (lambda ()
+;;        ;; We like nice colors
+;;        (ansi-color-for-comint-mode-on)
+;;        ;; Deal with some prompt nonsense
+;;        (add-to-list
+;;         'comint-preoutput-filter-functions
+;;         (lambda (output)
+;;           (replace-regexp-in-string "\033\\[[0-9]+[A-Z]" "" output)))))
 
 (define-minor-mode inferior-js-keys-mode
   "Bindings for communicating with an inferior js interpreter."
