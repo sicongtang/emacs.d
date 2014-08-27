e7dda31a5699f0a2fbc2a775557ea76620fb7123
sicongtang <bobby3239@gmail.com>
2014-08-28 01:35:04 +0800

    enable C-u to scroll-up

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -7,4 +7,9 @@
 (define-key evil-insert-state-map (kbd "M-e") 'move-end-of-line)
 (define-key evil-insert-state-map (kbd "C-k") 'kill-line)
 
+
+(define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
+(define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
+
+
 (provide 'init-evil)
