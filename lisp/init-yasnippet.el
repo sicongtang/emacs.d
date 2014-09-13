f249d5d01a214403cec802f34c4e6ae513d34a1c
sicongtang <bobby3239@gmail.com>
2014-09-13 16:20:43 +0800

    disable tab key in yasnippet

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -1,3 +1,4 @@
+;; https://github.com/capitaomorte/yasnippet
 (require-package 'yasnippet)
 
 ;; personal snippets --> https://github.com/AndreaCrotti/yasnippet-snippets
@@ -14,5 +15,15 @@
 ;; Add your own snippets to ~/.emacs.d/snippets by placing files there or invoking yas-new-snippet.
 
 (yas-global-mode 1)
+
+;;keys for navigation
+(define-key yas-keymap [(tab)]       nil)
+(define-key yas-keymap (kbd "TAB")   nil)
+(define-key yas-keymap [(shift tab)] nil)
+(define-key yas-keymap [backtab]     nil)
+(define-key yas-keymap (kbd "C-]") 'yas-next-field-or-maybe-expand)
+;;(define-key yas-keymap (kbd "C-[") 'yas-prev)
+
+(global-set-key (kbd "C-c y") 'yas-insert-snippet)
 
 (provide 'init-yasnippet)
