4434157ea88c568a565fb8a5d947ae89d5d489ae
sicongtang <bobby3239@gmail.com>
2014-11-13 21:23:52 +0800

    elpa folder commit

---
+++ elpa/flycheck-hdevtools-20140711.13/flycheck-hdevtools-pkg.el
@@ -0,0 +1,1 @@
+(define-package "flycheck-hdevtools" "20140711.13" "A flycheck checker for Haskell using hdevtools" (quote ((flycheck "0.15"))))
---
+++ elpa/frame-restore-20140705.112/frame-restore-pkg.el
@@ -0,0 +1,1 @@
+(define-package "frame-restore" "20140705.112" "Restore Emacs frame" (quote ((emacs "24.1"))))
